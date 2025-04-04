4f4fa14e41895eec3eae47af1a95299f6c7ad69d
Daimus Suudi <daimus.suudi@gmail.com>
2025-04-05 00:23:42 +0700

    feat: add logout button

--- src/app/task-manager.tsx
+++ src/app/task-manager.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import {GalleryVerticalEnd} from "lucide-react";
+import {GalleryVerticalEnd, LogOut} from "lucide-react";
 import {Card, CardContent} from "@/components/ui/card";
 import CreateTaskForm from "@/components/task/create-task-form";
 import TaskList from "@/components/task/task-list";
 import {SessionProvider} from "@/components/session-provider";
+import {Button} from "@/components/ui/button";
 
 export default function TaskManager ({accessToken} : {accessToken: string}){
     return (
@@ -12,13 +13,20 @@ export default function TaskManager ({accessToken} : {accessToken: string}){
             <SessionProvider accessToken={accessToken}>
                 <div className="flex flex-col items-center justify-center gap-6 p-6 md:p-10">
                     <div className="flex w-full max-w-2xl h-screen flex-col gap-6">
-                        <a href="#" className="flex items-center gap-2 self-center font-medium">
-                            <div
-                                className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-                                <GalleryVerticalEnd className="size-4"/>
-                            </div>
-                            Task Manager
-                        </a>
+                        <div className="flex w-full justify-between">
+                            <a href="#" className="flex items-center gap-2 self-center font-medium">
+                                <div
+                                    className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+                                    <GalleryVerticalEnd className="size-4"/>
+                                </div>
+                                Task Manager
+                            </a>
+                            <a href="/api/auth/logout">
+                                <Button variant="destructive" className="cursor-pointer">
+                                    <LogOut />
+                                </Button>
+                            </a>
+                        </div>
                         <div className="flex flex-col gap-6">
                             <Card className="h-full">
                                 <CardContent>
