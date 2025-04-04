4682668a228d19541481e85ce3825db2bf8b0456
Daimus Suudi <daimus.suudi@gmail.com>
2025-04-05 00:26:09 +0700

    refactor: change task loading logic

--- src/components/task/task-list.tsx
+++ src/components/task/task-list.tsx
@@ -7,13 +7,17 @@ import {useWatch} from "@/hooks/useWatch";
 
 export default function TaskList(){
     const {watch} = useWatch()
-    const {isLoading, data} = useFetch(`/api/v1/tasks?watch=${watch}`)
+    const {data} = useFetch(`/api/v1/tasks?watch=${watch}`)
+
+    if (!data){
+        return <div className="mt-4">
+            <TaskLoading />
+        </div>
+    }
+
     return (
         <>
             <div className="mt-4">
-                {
-                    isLoading && <TaskLoading />
-                }
                 <div className="grid gap-2">
                     {
                         (data || []).map(task => (
