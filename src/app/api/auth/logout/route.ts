c7d1cdda1cc4c5f7586bd8e147c8642b59935185
Daimus Suudi <daimus.suudi@gmail.com>
2025-04-06 23:38:42 +0700

    refactor: execute logout API when user logout

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -3,9 +3,12 @@ import {cookies} from "next/headers";
 import { redirect } from 'next/navigation'
 import {ISessionData} from "@/type/session";
 import {sessionConfig} from "@/constant/session";
+import axios from "axios";
+import {createAxiosConfig} from "@/lib/utils";
 
 export async function GET() {
     const session = await getIronSession<ISessionData>(await cookies(), sessionConfig);
-    await session.destroy();
+    await axios.post('/api/v1/auth/logout', {}, createAxiosConfig(session.access_token));
+    session.destroy();
     redirect('/login')
 }
